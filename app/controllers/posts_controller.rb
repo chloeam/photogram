46a97ce595cad1730d7d3595ff9aed54ae620069
Chloe Atchue-Mamlet <chloe793@gmail.com>
2015-12-24 15:57:26 -0600

    add flashes to tell user of success and failure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,20 +14,31 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created. Please check the form."
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
-    @post.update(post_params)
-    redirect_to (post_path(@post))
+    if @post.update(post_params)
+      flash[:success] = "Your post has been updated!"
+      redirect_to (post_path(@post))
+    else
+      flash.now[:alert] = "Update failed. Please check the form."
+      render :edit
+    end
   end
 
   def destroy
     @post.destroy
+    flash[:success] = "Your post has been deleted."
     redirect_to posts_path
   end
 
